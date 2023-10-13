7a35c574d4e9f4009209484a739645e6db37d40b
Brian McDonald <brian.mcdonald@falmouth.ac.uk>
2023-10-14 00:12:33 +0100

    Added comments and shifted some code around

--- source/main.lua
+++ source/main.lua
@@ -17,7 +17,7 @@ local conversation_sound=nil
 -- https://www.rightchannelradios.com/blogs/newsletters/cb-radio-frequencies-and-channels#:~:text=The%20CB%20Radio%20spectrum%20is,noted%20in%20the%20table%20below.
 
 
-
+--load game
 local function loadGame()
 	playdate.display.setRefreshRate(50) -- Sets framerate to 50 fps
 	math.randomseed(playdate.getSecondsSinceEpoch()) -- seed for math.random
@@ -35,11 +35,16 @@ local function loadGame()
 	end
 end
 
+--update game
 local function updateGame()
+	--get crank ticks, this will update every 90 degrees( 360 / 4 )
 	angleChange=playdate.getCrankTicks(4)
+	-- change the logical channel and frequency based on the crank ticks
 	crankValueDelta=angleChange
 	currentChannel+=crankValueDelta
+	-- frequency is 26.965 to 27.405 and will go up in 10khz increments
 	frequency+=crankValueDelta*0.01
+	-- test stuff to work on interrupting background sound with conversation sound  (will be reworked)
 	if (currentChannel==testSoundChannel) then
 		
 		if (background_sound:isPlaying()) then
@@ -59,16 +64,21 @@ local function updateGame()
 
 end
 
+-- draw game
 local function drawGame()
 	gfx.clear() -- Clears the screen
+	playdate.drawFPS(0,0) -- FPS widget
+	-- draw the frequency capped at 3 decimal places
+	gfx.drawText(string.format("%.3f", frequency),180,110)
+	-- debug crank value
+	gfx.drawText(crankValueDelta,300,0)
 end
 
+-- call load game function
 loadGame()
 
+-- call back update function, game loop
 function playdate.update()
 	updateGame()
 	drawGame()
-	playdate.drawFPS(0,0) -- FPS widget
-	gfx.drawText(string.format("%.3f", frequency),180,110)
-	gfx.drawText(crankValueDelta,300,0)
 end
