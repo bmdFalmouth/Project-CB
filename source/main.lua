0ede015af803174cf663715213a77b47df9565f9
Brian McDonald <brian.mcdonald@falmouth.ac.uk>
2023-10-14 00:58:20 +0100

    Removed frequencies and went with channels instead

--- source/main.lua
+++ source/main.lua
@@ -3,7 +3,7 @@ import "CoreLibs/object"
 import "CoreLibs/crank"
 
 local gfx <const> = playdate.graphics
-local frequency=26.965
+--local frequency=26.965
 local crankValueDelta=0
 
 local currentChannel=0
@@ -16,6 +16,13 @@ local conversation_sound=nil
 -- CB Radio frequencies
 -- https://www.rightchannelradios.com/blogs/newsletters/cb-radio-frequencies-and-channels#:~:text=The%20CB%20Radio%20spectrum%20is,noted%20in%20the%20table%20below.
 
+--from https://devforum.play.date/t/a-list-of-helpful-libraries-and-code/221/18
+function math.clamp(a, min, max)
+    if min > max then
+        min, max = max, min
+    end
+    return math.max(min, math.min(max, a))
+end
 
 --load game
 local function loadGame()
@@ -38,12 +45,9 @@ end
 --update game
 local function updateGame()
 	--get crank ticks, this will update every 90 degrees( 360 / 4 )
-	angleChange=playdate.getCrankTicks(4)
-	-- change the logical channel and frequency based on the crank ticks
-	crankValueDelta=angleChange
+	crankValueDelta=playdate.getCrankTicks(2)
 	currentChannel+=crankValueDelta
-	-- frequency is 26.965 to 27.405 and will go up in 10khz increments
-	frequency+=crankValueDelta*0.01
+	currentChannel=math.clamp(currentChannel,0,40)
 	-- test stuff to work on interrupting background sound with conversation sound  (will be reworked)
 	if (currentChannel==testSoundChannel) then
 		
@@ -69,7 +73,7 @@ local function drawGame()
 	gfx.clear() -- Clears the screen
 	playdate.drawFPS(0,0) -- FPS widget
 	-- draw the frequency capped at 3 decimal places
-	gfx.drawText(string.format("%.3f", frequency),180,110)
+	gfx.drawText(currentChannel,180,110)
 	-- debug crank value
 	gfx.drawText(crankValueDelta,300,0)
 end
