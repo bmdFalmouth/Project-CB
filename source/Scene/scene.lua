bdf5fa4b5084f5dea796504640cc3951569b697e
Brian McDonald <brian.mcdonald@falmouth.ac.uk>
2023-10-21 22:46:23 +0100

    Merged local changes

--- source/Scene/scene.lua
+++ source/Scene/scene.lua
@@ -1,11 +1,12 @@
 import 'CoreLibs/object'
 
-class('Scene',{renderQueue={}}).extends()
+class('Scene',{renderQueue={},nextScene=nil}).extends()
 
 
 function Scene:init()
     Scene.super.init(self)
     self.renderQueue = {}
+    self.nextScene = nil
 end
 
 function Scene:load()
@@ -25,5 +26,42 @@ end
 function Scene:update()
     for i=1,#self.renderQueue do
         self.renderQueue[i]:update()
+    end
+end
+
+function Scene:setNextScene(scene)
+    self.nextScene=scene
+end
+
+class('SceneManager',{sceneList={},currentScene=nil}).extends()
+
+function SceneManager:init()
+    SceneManager.super.init(self)
+    self.sceneList={}
+    self.currentScene=nil
+end
+
+function SceneManager:addScene(scene)
+    table.insert(self.sceneList,scene)
+    self.setCurrentScene(self.sceneList[1])
+end
+
+function SceneManager:setCurrentScene(scene)
+    self.currentScene=scene
+end
+
+function SceneManager:getCurrentScene()
+    return self.currentScene
+end
+
+function SceneManager:update()
+    if self.currentScene~=nil then
+        self.currentScene:update()
+    end
+end
+
+function SceneManager:draw(gfx)
+    if self.currentScene~=nil then
+        self.currentScene:draw(gfx)
     end
 end
--- source/main.lua
+++ source/main.lua
@@ -3,14 +3,12 @@ import "CoreLibs/object"
 import "CoreLibs/crank"
 import "Math/math"
 
-
 import "Sprite/sprite"
 import "Project-CB"
 
 local gfx <const> = playdate.graphics
 
 local gameScene=nil
-
 
 -- CB Radio frequencies
 -- https://www.rightchannelradios.com/blogs/newsletters/cb-radio-frequencies-and-channels#:~:text=The%20CB%20Radio%20spectrum%20is,noted%20in%20the%20table%20below.
@@ -23,11 +21,14 @@ local function loadGame()
 	
 	gameScene=ProjectCBScene()
 	gameScene:load()
+
 end
 
 --update game
 local function updateGame()
+
 	gameScene:update()
+
 end
 
 -- draw game
@@ -36,9 +37,6 @@ local function drawGame()
 	
 	gfx.setFont(systemFont) -- DEMO
 	playdate.drawFPS(0,0) -- FPS widget
-	-- debug crank value
-	--debugText:draw(gfx)
-	--currentChannelText:draw(gfx)
 	gameScene:draw(gfx)
 
 end
