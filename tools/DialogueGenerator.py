250a2e621930e17cddc257cb945aed1026626065
Brian McDonald <brian.mcdonald@falmouth.ac.uk>
2024-07-18 21:16:21 +0100

    Dialogue generator now working

--- tools/DialogueGenerator.py
+++ tools/DialogueGenerator.py
@@ -1,8 +1,11 @@
+#might use https://github.com/neonbjb/tortoise-tts to replace google
 from gtts import gTTS
 from pydub import AudioSegment
 
+
 import openpyxl
 import os
+import shutil
 
 def main():
     wb=None
@@ -11,8 +14,10 @@ def main():
     except:
         print("Error: Cannot open the excel file")
         return
-    
-    os.makedirs('/temp/Sound', exist_ok=True)
+    #temp path
+    tempPathRoot=os.path.join(os.getcwd(),'temp')
+    tempPath=os.path.join(tempPathRoot,'Sound')
+    os.makedirs(tempPath, exist_ok=True)
     #open the sheet
     sheet = wb['Sheet1']
     for row in sheet.iter_rows(min_row=2, max_row=sheet.max_row, min_col=1, max_col=4, values_only=True):
@@ -22,15 +27,15 @@ def main():
         
         # create temp folder in the root directory
         
-        tts.save(f'/temp/Sound/{sound_file_name}.mp3')
+        tts.save(f'{tempPath}/{sound_file_name}.mp3')
         print(f"Sound file {sound_file_name} generated")
 
         #convert the mp3 file to wav
-        sound = AudioSegment.from_mp3(f'/temp/Sound/{sound_file_name}.mp3')
+        sound = AudioSegment.from_mp3(f'{tempPath}/{sound_file_name}.mp3')
         sound.export(f'../source/sound/{sound_file_name}.wav', format="wav")
         
     #delete the temp folder and all files in it
-    os.system('rm -rf /temp')
+    shutil.rmtree(tempPathRoot)
 
 
 if __name__ == "__main__":
