69a7dc5af8a62d093b1d6f472bdc8a2d3c895866
Brian McDonald <brian.mcdonald@falmouth.ac.uk>
2024-07-22 22:52:47 +0100

    future proofing tools

--- tools/DialogueGenerator.py
+++ tools/DialogueGenerator.py
@@ -19,7 +19,7 @@ def main():
     tempPath=os.path.join(tempPathRoot,'Sound')
     os.makedirs(tempPath, exist_ok=True)
     #open the sheet
-    sheet = wb['Sheet1']
+    sheet = wb['index']
     for row in sheet.iter_rows(min_row=2, max_row=sheet.max_row, min_col=1, max_col=4, values_only=True):
         game_time, assigned_channel, story_text,sound_file_name = row
         print(f"time: {game_time} channel: {assigned_channel} story: {story_text} sound: {sound_file_name}")
